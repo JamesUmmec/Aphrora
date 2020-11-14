12e094e4e0ded5ddad62c0ca4db1cabead736d70
JamesUmmec <DreamingBoat@outlook.com>
2020-11-14 11:35:54 +0800

    run server closure space

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
-pub mod http;
+use std::net::TcpListener;
 
-pub fn run_server() {}
+pub mod http;
+use http::{ Request, Response };
+
+pub fn run_server<F>(views_handler: F)
+    where F: Fn(Request) -> Response, F: Send + Copy + 'static {
+    // coding here
+}
