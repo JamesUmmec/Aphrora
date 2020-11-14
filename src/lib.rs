c7cf2f998ee88d34c2c80d87f2f32400c65e1d0e
JamesUmmec <DreamingBoat@outlook.com>
2020-11-14 12:04:55 +0800

    launch server by TcpListener.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, SocketAddr, IpAddr, Ipv4Addr};
 
 pub mod http;
 use http::{ Request, Response };
 
 pub fn run_server<F>(views_handler: F)
     where F: Fn(Request) -> Response, F: Send + Copy + 'static {
-    // coding here
+
+    // port 0 means allocate port automatically by the system.
+    let localhost = SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::new(127,0,0,1)), 0);
+    // bind port and launch server.
+    let listener = TcpListener::bind(localhost)
+        .expect("Cannot allocate available port.");
 }
