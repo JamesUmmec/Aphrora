5493d0895a47a3a29e66b5fd974c942311155dd0
JamesUmmec <DreamingBoat@outlook.com>
2020-11-14 11:26:34 +0800

    init http.rs basic structure.

--- src/http.rs
+++ src/http.rs
@@ -1,1 +1,46 @@
+pub struct Request {
+    method: RequestMethod,
+    view: String,
+    message: String,
+}
 
+pub enum RequestMethod {
+    GET,
+    POST,
+    Unsupported,
+}
+
+impl Request {
+    pub fn from(request_string: String) -> Request {
+        // unfinished
+        println!(request_string); // delete this line
+
+        Request::default_unsupported()
+    }
+
+    pub fn default_unsupported() -> Request { Request {
+        method: RequestMethod::Unsupported,
+        view: String::new(),
+        message: String::new(),
+    } }
+}
+
+pub struct Response {
+    status: ResponseStatus,
+    message: String,
+}
+
+pub enum ResponseStatus {
+    OK,
+    NotFound,
+    Forbidden,
+    InternalServerError,
+}
+
+impl Response {
+    pub fn to_string(&self) -> String {
+        // unfinished
+
+        String::new()
+    }
+}
