23e2cadcef0bdb7a4906a245a40667126f05bf7a
JamesUmmec <DreamingBoat@outlook.com>
2020-11-14 12:59:08 +0800

    open in default browser

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use std::net::{TcpListener, SocketAddr, IpAddr, Ipv4Addr};
+use std::process::Command;
+use std::error::Error;
 
 pub mod http;
 use http::{ Request, Response };
+use std::borrow::Borrow;
 
 pub fn run_server<F>(views_handler: F)
     where F: Fn(Request) -> Response, F: Send + Copy + 'static {
@@ -12,4 +15,67 @@ pub fn run_server<F>(views_handler: F)
     // bind port and launch server.
     let listener = TcpListener::bind(localhost)
         .expect("Cannot allocate available port.");
+
+    // open in default browser.
+    try_open_in_browser(listener.borrow());
+}
+
+/// Try open the root view of the `listener` in web browser.<br>
+/// 尝试在浏览器中打开这个`listener`所对应的地址。<br><br>
+///
+/// If it cannot be opened in the browser automatically,
+/// the address will be log out to let user to open it
+/// in the browser manually.<br>
+/// 如果无法自动在浏览器中打开，
+/// 将会在控制台输出对应的网址，
+/// 用户可以手动在浏览器中打开这个网址。<br><br>
+fn try_open_in_browser(listener_borrow: &TcpListener) {
+    // use listener_borrow.local_addr() rather than 127.0.0.1:0 here.
+    let address_url = listener_borrow.local_addr()
+        .expect("Cannot get server address.").to_string();
+    let address_url = address_url.as_str();
+
+    match try_call_system_command(address_url) {
+        Ok(()) => println!("Opened in browser successfully."),
+        Err(_) => println!(
+            "Cannot open in browser automatically.\n\
+            Please visit {} in your browser.", address_url
+        )
+    }
+
+    /// Try call system to open an address in browser.<br>
+    /// 尝试调用系统在浏览器中打开网址。<br><br>
+    ///
+    /// # Details 细格
+    /// *   Now only `windows` `macos` `linux` are supported.
+    ///     And only `windows` is fully supported.
+    ///     ( The development of support for other operation
+    ///     systems are not finished yet. )
+    /// *   In `linux`, as there might be no browser or no default browser,
+    ///     it will try `firefox` first, rather than `chrome`,
+    ///     because `firefox` is also developed by `mozilla`.
+    /// *   仅 `windows` `macos` 和 `linux` 系统是受到支持的，
+    ///     并且仅 `windows` 是受到稳定支持的，其他系统还没有适配好。
+    /// *   在 `linux` 系统中，可能没有浏览器或没有默认浏览器。
+    ///     这个程序首先会尝试火狐浏览器，而不是谷歌浏览器，
+    ///     因为火狐和 Rust 编程语言都是 Mozilla 开发的。(2333)
+    fn try_call_system_command(address: &str)
+        -> Result<(), Box<dyn Error>> {
+
+        match std::env::consts::OS {
+            // try using "cmd" in Windows Operation System
+            "windows" => Command::new("cmd")
+                .arg("start").arg(address).output()?,
+
+            // try using terminal in Mac Operation System
+            "macos" => Command::new("terminal")
+                .arg("open").arg(address).output()?,
+
+            // not Windows and not MacOS,
+            // so it is most likely to be Linux.
+            // If not Linux, it is not supported yet.
+            _ => Command::new("terminal")
+                .arg("firefox").arg(address).output()?,
+        }; Ok(())
+    }
 }
