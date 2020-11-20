11a61348b498a6718af1e4ee86cf07c568fe5eaf
JamesUmmec <DreamingBoat@outlook.com>
2020-11-20 17:37:20 +0800

    add comments

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+/// Try get pages file under a root path,
+/// which enable approach to different pages easier.
 pub fn try_under_root(root_path: &str, file_path: &str) -> Option<String> {
     match fs::read_to_string(root_path.to_owned() + file_path) {
         Ok(file_string) => Option::Some(file_string),
